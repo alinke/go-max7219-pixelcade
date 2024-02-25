a3de391efdbe140b2949ea331b65c5cd34bf8891
Al Linke <al@pixelcade.org>
2024-02-25 11:37:46 -0800

    updated example code to scroll text and show still

--- examples/slide-8in1-and-still-text.go
+++ examples/slide-8in1-and-still-text.go
@@ -5,65 +5,53 @@ and if the number of characters is less than x, the text will not scroll
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 	"time"
 
 	"github.com/alinke/go-max7219-pixelcade"
 )
 
 func main() {
-
-	var err error
 	var message string
-	var loops int
-	var modules int
+	var loops, modules int
 
-	args := os.Args
-	fmt.Println("Args length", len(args))
-	if len(args) < 2 {
-		fmt.Println("Usage: <message> <loops> <modules> [speed(ms)]")
+	flag.StringVar(&message, "message", "", "Message to display")
+	flag.IntVar(&loops, "loops", 1, "Number of loops")
+	flag.IntVar(&modules, "modules", 8, "Number of max7219 modules")
+	flag.Parse()
+
+	if message == "" {
+		fmt.Println("Usage: -message <message> -loops <loops> -modules <modules>")
 		os.Exit(1)
-	} else {
-		if len(args) < 3 { // we only have message
-			message = args[1]
-			fmt.Println("Loops not entered, defaulting to 1")
-			loops = 1
-			fmt.Println("Num modules not entered, defaulting to 8")
-			modules = 8
-		} else { //let's assume we have loops and modules then
-			message = args[1]
+	}
 
-			loops, err = strconv.Atoi(args[2])
-			if err != nil || loops < 1 {
-				fmt.Println("Loops not entered, defaulting to 1")
-				loops = 1
-			}
+	if loops < 1 {
+		fmt.Println("Invalid loop count. Defaulting to 1.")
+		loops = 1
+	}
 
-			modules, err = strconv.Atoi(args[3])
-			if err != nil || modules < 1 {
-				fmt.Println("Num modules not entered, defaulting to 8")
-				modules = 8
-			}
-		}
+	if modules < 1 {
+		fmt.Println("Invalid number of modules. Defaulting to 8.")
+		modules = 8
 	}
 
 	mtx := max7219.NewMatrix(8, max7219.RotateClockwiseInvert)
-	err = mtx.Open(1, 1, 7)
+	err := mtx.Open(1, 1, 7)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	if len(message) <= modules {
-		fmt.Println("Displaying without scrolling: ", message)
+		//fmt.Println("Displaying without scrolling: ", message)
 		startPos := (modules - len(message)) / 2
 		for i, char := range message {
-			mtx.OutputChar(i+startPos, max7219.FontLCD, char, true) //FontSinclair or FontLCD
+			mtx.OutputChar(i+startPos, max7219.FontLCD, char, true) // FontSinclair or FontLCD
 		}
 	} else {
-		fmt.Printf("Scrolling message: %s, Loops: %d, Modules: %d, Delay: %dms\n", message, loops, modules)
+		//fmt.Printf("Scrolling message: %s, Loops: %d, Modules: %d\n", message, loops, modules)
 		for i := 0; i < loops; i++ {
 			mtx.SlideMessage(message, max7219.FontLCD, true, 25*time.Millisecond)
 		}
